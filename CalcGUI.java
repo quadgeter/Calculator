81227b8227cfab75ecfa70245c790f6e772e19ee
quadgeter <116421941+quadgeter@users.noreply.github.com>
2023-04-24 12:56:27 -0400

    GUI class, main class, operations class

---
+++ CalcGUI.java
@@ -0,0 +1,9 @@
+
+public class CalcGUI extends Operations{
+	
+	public static void main(String[] args) {
+		CalculatorGUI calculator = new CalculatorGUI();
+		
+	}
+
+}
---
+++ Operations.java
@@ -0,0 +1,23 @@
+
+public class Operations {
+
+	public static int add(int num1, int num2) {
+		return num1 + num2;
+	}
+	
+	public static int subtract(int num1, int num2) {
+		return num1 - num2;
+	}
+	
+	public static int multiply(int num1, int num2) {
+		return num1 * num2;
+	}
+	
+	public static int divide(int num1, int num2) {
+		return num1 / num2;
+	}
+	
+	public static double toPercent(double num1) {
+		return num1 * 100;
+	}
+}
